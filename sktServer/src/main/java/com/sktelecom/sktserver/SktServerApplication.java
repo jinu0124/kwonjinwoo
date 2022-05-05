71478932bb5e27a2a9f4de5a2371c3ff09763c48
jinu0124 <jinwoo6612@naver.com>
2022-05-05 11:07:51 +0900

    Initial commit

---
+++ sktServer/src/main/java/com/sktelecom/sktserver/SktServerApplication.java
@@ -0,0 +1,13 @@
+package com.sktelecom.sktserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SktServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SktServerApplication.class, args);
+    }
+
+}
---
+++ sktServer/src/test/java/com/sktelecom/sktserver/SktServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sktelecom.sktserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SktServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
